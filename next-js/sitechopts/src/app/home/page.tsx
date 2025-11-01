26aa1a1e6d1f2d3f92eaba2534646507dacfe3e8
fonsecak <derygo3015@gmail.com>
2025-10-31 22:11:26 -0300

    aula 31-10

---
+++ next-js/sitechopts/src/app/home/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+
+    return (
+        <main>
+            <h1>Welcome to the Home Page</h1>
+        </main>
+
+    )
+}
---
+++ next-js/sitechopts/src/app/produtos/[id]/page.tsx
@@ -0,0 +1,22 @@
+import styles from "./style.module.css"
+
+interface IParametro {
+    params: { id: string }
+}
+
+export default async function Categorias({ params }: IParametro) {
+
+    const { id } = await params
+
+    return (
+        <>
+            <h1 style={{
+                color: 'red'
+            }}>
+                Parametro: {id}</h1>
+            <h1 className="bg-red-100">CSS</h1>
+            <h2 className={styles.subtitulo}>Subtitulo</h2>
+        </>
+    )
+
+}
